e45c15d9f5c65e0922cce37bf4ca8ed630cf32e8
Jason Kurtz <jason@jkurtz.net>
2014-08-02 11:10:25 -0400

    doesn't work, i wonder why

--- python/Pythag.py
+++ python/Pythag.py
@@ -11,9 +11,20 @@ class Pythag:
     def doHyp(self, width, height): 
         return math.sqrt(width ** 2 + height ** 2)
 
+    def wholeHyp(self): 
+        hyps = [ ]
+        for i in range(1, 100): 
+            for j in range(2, 100):
+                if self.doHyp(i, j).is_integer() and i < j: 
+                    hyps.append([i, j, self.doHyp(i, j)])
+
+        for h in hyps: 
+            print "%f, %f = %f" % h[0], h[1], h[2]
+
         
 P = Pythag(3, 4)
 print P.getHyp()
+print P.wholeHyp()
 
 
  
