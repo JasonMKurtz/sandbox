3903b2d90fdd166855ff214fc8e1c8563288268f
Jason Kurtz <jason.kurtz@jcrew.com>
2015-10-22 13:22:24 -0400

    lambda sort/count

---
+++ python/lambda_sort.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+""" Requires python2.7 """ 
+import operator
+i = 1000
+input = [ x for x in xrange(0, i) ]
+n = filter(lambda x: (x % 5) != 0 and (x % 4) != 0 and len(set(str(x))) == len(str(x)), input)
+count = { i: 0 for i in set([ len(str(x)) for x in n ]) }
+for i in n: count[len(str(i))] += 1
+                                             
+for i in sorted(count.items(), key=operator.itemgetter(1))[::-1]: 
+    print "%d digits: %d" % (i[0], i[1])
