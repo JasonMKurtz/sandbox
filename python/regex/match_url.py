823d0475bdaca72a0c4a62fa86a7f4ba8042e2e4
Jason Kurtz <jason.kurtz@jcrew.com>
2016-02-26 14:09:33 -0500

    minimalism.

--- python/regex/match_url.py
+++ python/regex/match_url.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import re, sys
-regex = "(?P<url>:?(?P<proto>https?)?:?\/?\/?(?P<hostname>[^\/:]+))\/?:?(?P<port>[0-9]+)?(?P<path>\/.+)?"
+regex = "(?P<url>:?(?P<proto>https?)?:?\/?\/?(?P<hostname>[^\/:]+)):?(?P<port>[0-9]+)?(?P<path>.+)?"
 #string = 'https://nyc-wdevtool-01:8080/jenkins/job/ecommerce-platform-master-appc/1679/'
 string = ' '.join(sys.argv[1:])
 
 m = re.search(regex, string)
 print string
-#print "URL: %s, protocol: %s, hostname: %s, port: %s, path: %s" % (m.group('url'), "http" if m.group('proto') is None else m.group('proto'), m.group('hostname'), "80" if m.group('port') is None else m.group('port'), m.group('path'))
 print m.groupdict()
