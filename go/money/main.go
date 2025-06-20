a625ef8eac971878907997cf38995471b9dd8442
Jason Kurtz <jason@jkurtz.net>
2025-06-20 13:14:20 -0400

    Trying out the plaid API

---
+++ go/money/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/plaid/plaid-go/v20/plaid"
+)
+
+type Manager struct {
+	apiclient *client
+	accounts  map[string]string
+}
+
+func NewManager(env plaid.Environment) (*Manager, error) {
+	m := &Manager{}
+
+	m.apiclient = NewClient()
+	if err := m.apiclient.Init(env); err != nil {
+		return nil, fmt.Errorf("error creating manager: %e", err)
+	}
+
+	names, err := m.apiclient.getAccountNames()
+	if err != nil {
+		return nil, err
+	}
+	m.accounts = names
+
+	return m, nil
+}
+
+func main() {
+	m, err := NewManager(plaid.Sandbox)
+	if err != nil {
+		log.Fatalf("error initializing money manager: %e", err)
+	}
+
+	time.Sleep(4 * time.Second)
+
+	transactions, err := m.GetTransactions()
+	if err != nil {
+		log.Fatalf("error reading transactions: %e", err)
+	}
+
+	fmt.Printf("%s\n", transactions[0])
+}
